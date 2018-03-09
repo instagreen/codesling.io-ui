5c86fe558f1675eddcf06ef9703391396f0a6247
Antonio <toncas@windowslive.com>
2018-03-09 15:48:14 -0800

    [Friends list] Can now render friendslist

--- client/src/components/Friends/FriendSearch.jsx
+++ client/src/components/Friends/FriendSearch.jsx
@@ -4,9 +4,9 @@ import FriendListEntry from './FriendsListEntry.jsx';
 const FriendSearch = (props) => (
   <div className='friend-list'>
     <h4>Results: </h4>
-    <ul>
+    <select size="3">
       {props.searchList.map((user) => <FriendListEntry key={user.id} user={user}/>)}
-    </ul>
+    </select>
   </div>
 );
 
--- client/src/components/Friends/FriendsList.jsx
+++ client/src/components/Friends/FriendsList.jsx
@@ -4,10 +4,10 @@ import FriendListEntry from './FriendsListEntry.jsx';
 const FriendsList = (props) => (
   <div className='friend-list'>
     <h4>Friends: </h4>
-    <ul>
-      {props.friends.map((friend) => <FriendListEntry key={user.id} user={friend}/>)}
-    </ul>
-  </div>
+    <select size="5">
+      {props.friends.map((friend) => <FriendListEntry key={friend.id} user={friend}/>)}
+    </select>
+  </select>
 );
 
 export default FriendsList;
